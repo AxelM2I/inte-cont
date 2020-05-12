3ac06f3a6967e899fcf73efe880c77bde7572fa1
Axel ELAIN <axel.elain@labom2iformation.onmicrosoft.com>
2020-05-12 16:31:47 +0200

    Ajout du projet de TU

---
+++ inte-cont-UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("inte-cont-UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("inte-cont-UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("544feb7b-3349-4caf-9e8d-159063f24a10")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ inte-cont-UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using inte_conti;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace inte_cont_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var calc = new ConverterLibrary();
+            // Act
+            var result = calc.GetInt("42");
+            //Assert
+            Assert.AreEqual(42, result);
+        }
+    }
+}
